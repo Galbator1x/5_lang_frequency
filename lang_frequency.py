e6aeae4bd9beba359d1794875805cfd9a792e307
Ivan Filimonov <snake73w@gmail.com>
2016-09-29 21:17:52 +0700

    set defalut encode; added exit of the script if the input file does not exists.

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,13 +5,14 @@ import operator
 
 def load_data(filepath):
     if not os.path.exists(filepath):
-        return None
-    with open(filepath, encoding='cp1251') as file_handler:
+        print('File does not exists.')
+        exit()
+    with open(filepath) as file_handler:
         return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    words = re.findall(r'\w+', text)
+    words = re.findall(r'\w+', text.lower())
     words_count = {}
     for word in words:
         words_count[word] = words_count.get(word, 0) + 1
